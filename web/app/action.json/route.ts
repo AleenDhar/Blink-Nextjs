4c2d8c53342f390c6aff16481b1b49e7e580f80c
Aleen Dhar <aleendhar23@gmail.com>
2024-09-06 00:12:54 +0530

    the blink link

--- web/app/action.json/route.ts
+++ web/app/action.json/route.ts
@@ -6,7 +6,7 @@ export const GET = async () => {
       // map all root level routes to an action
       {
         pathPattern: "/blink",
-        apiPath: "https://www.gitblocks.co/api/donate-sol",
+        apiPath: "/api/donate-sol",
       },
       // idempotent rule as the fallback
     //   {
